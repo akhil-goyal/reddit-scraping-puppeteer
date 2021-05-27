8676d7c22c3b55b01277a69384443d6d4addf79b
Akhil Goyal <akhilgoyal@Akhils-MacBook-Pro.local>
2021-05-27 16:06:46 -0400

    Added functionality to scrape comments text & points from reddit.

--- index.js
+++ index.js
@@ -15,20 +15,40 @@ const URL = `https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_
 
     await page.goto(URL);
 
-    let expandButtons = await page.$$('.morecomments');
+    // let expandButtons = await page.$$('.morecomments');
 
-    while (expandButtons.length) {
+    // while (expandButtons.length) {
 
-        for (let button of expandButtons) {
-            await button.click();
-            await page.waitForTimeout(500);
+    //     for (let button of expandButtons) {
+    //         await button.click();
+    //         await page.waitForTimeout(500);
+    //     }
+
+    //     await page.waitForTimeout(1000);
+
+    //     expandButtons = await page.$$('.morecomments');
+
+    // }
+
+    const comments = await page.$$('.entry');
+
+    const formattedComments = [];
+
+    for (let comment of comments) {
+
+        const points = await comment.$eval('.score', el => el.textContent).catch(err => console.error('No score'));
+
+        const rawText = await comment.$eval('.usertext-body', el => el.textContent).catch(err => console.error('No Text'));
+
+        if (points && rawText) {
+
+            const text = rawText.replace(/\n/g, '');
+
+            formattedComments.push({ points, text });
         }
-
-        await page.waitForTimeout(1000);
-
-        expandButtons = await page.$$('.morecomments');
-
     }
+
+    console.log({ formattedComments });
 
     await browser.close();
 
